a66bfec0301ce621717666431ac78af26339ca20
viv448 <54100077+viv448@users.noreply.github.com>
2022-11-22 17:25:38 +0000

    Initial commit

---
+++ modelFormsDemo/modelForms/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from modelForms.forms import ProjectForm
+from modelForms.models import Project
+# Create your views here.
+
+def index(request):
+    return render(request,'modelForms/index.html')
+
+
+def listProjects(request):
+    projectsList = Project.objects.all()
+    return render(request, 'modelForms/listProjects.html', {'Projects':projectsList})
+
+
+def addProject(request):
+    form = ProjectForm()
+    if request.method=='POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return index(request)
+    return render(request, 'modelForms/addProject.html', {'Form': form})
+
+

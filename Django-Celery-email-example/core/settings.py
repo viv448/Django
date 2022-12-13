f08f08cab217f07cf1559356492e90ed8daee27a
viv448 <54100077+viv448@users.noreply.github.com>
2022-12-13 18:04:08 +0000

    Second commit

--- Django-Celery-email-example/core/settings.py
+++ Django-Celery-email-example/core/settings.py
@@ -124,8 +124,8 @@ STATIC_URL = '/static/'
 # gmail_send/settings.py
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'veryacademydemo@gmail.com'
-EMAIL_HOST_PASSWORD = 'jaazriuaslxfpwpg'
+EMAIL_HOST_USER = 'vivygal14@gmail.com'
+EMAIL_HOST_PASSWORD = 'puholsfeiblmtmrl'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'veryacademydemo@gmail.com'
+DEFAULT_FROM_EMAIL = 'vivygal14@gmail.com'
--- Django-Celery-email-example/task2/tasks.py
+++ Django-Celery-email-example/task2/tasks.py
@@ -1,4 +1,5 @@
-from celery.decorators import task
+# from celery.decorators import task
+from celery import shared_task
 from celery.utils.log import get_task_logger
 
 from .email import send_review_email
@@ -6,7 +7,7 @@ from .email import send_review_email
 logger = get_task_logger(__name__)
 
 
-@task(name="send_review_email_task")
+@shared_task(name="send_review_email_task")
 def send_review_email_task(name, email, review):
     logger.info("Sent review email")
     return send_review_email(name, email, review)
